b2fb61119bd0b34a6b6e91256850cf6c24dce312
Jingjing Bao <JBao6@slb.com>
2024-02-14 12:18:51 +0800

    add

---
+++ configuration/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace configuration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
