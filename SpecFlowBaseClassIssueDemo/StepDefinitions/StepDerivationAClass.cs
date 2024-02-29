ddf2925192897cc041c3790edca09a21d21a97fc
Jingjing Bao <JBao6@slb.com>
2024-02-29 13:14:28 +0800

    specflow base class ambiguous steps

---
+++ SpecFlowBaseClassIssueDemo/StepDefinitions/StepDerivationAClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlowBaseClassIssueDemo.StepDefinitions
+{
+    [Binding]
+    public class StepDerivationAClass: StepBaseClass
+    {
+        [Given(@"boz")]
+        public void GivenBoz()
+        {
+            Console.WriteLine("this is step boz");
+        }
+
+    }
+}
---
+++ SpecFlowBaseClassIssueDemo/StepDefinitions/StepDerivationBClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlowBaseClassIssueDemo.StepDefinitions
+{
+    [Binding]
+    public class StepDerivationBClass: StepBaseClass
+    {
+        [Given(@"bar")]
+        public void GivenBar()
+        {
+            Console.WriteLine("this is step bar");
+        }
+
+    }
+}
