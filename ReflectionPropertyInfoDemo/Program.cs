a55011189a9ab7334db5148e7b98ec952028af89
Jingjing Bao <JBao6@slb.com>
2024-03-15 16:44:43 +0800

    add

---
+++ ReflectionPropertyInfoDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ReflectionPropertyInfoDemo
+{
+    using System;
+    using System.Reflection;
+
+    public class MyClass
+    {
+        public string MyProperty { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            MyClass obj = new MyClass();
+            obj.MyProperty = "Hello World!";
+
+            // Get the PropertyInfo for the property
+            PropertyInfo propertyInfo = typeof(MyClass).GetProperty("MyProperty");
+
+            // Get the value of the property
+            object value = propertyInfo.GetValue(obj);
+
+            Console.WriteLine(value);  // Output: Hello, World!
+        }
+    }
+}
