cee2d0682aa8d3b6e74c83012f181ce3d5ab1860
Jingjing Bao <JBao6@slb.com>
2025-07-07 10:49:00 +0800

    add

---
+++ JObjectTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Slb.Cementing.Contracts.V1.DigitalCementPlan;
+using System.IO;
+// See https://aka.ms/new-console-template for more information
+class Program
+{
+
+
+    static void Main(string[] args)
+    {
+        
+        string inputFilePath = @"C:\Users\jbao6\Desktop\dev\cementing\DigitalCementPlan.json";
+        var inputcontent = File.ReadAllText(inputFilePath);
+        // var dcp = JsonConvert.DeserializeObject<DigitalCementPlanDto>(inputcontent);
+        // JsonConvert.SerializeObject(dcp);
+        JObject obj = JObject.Parse(inputcontent);
+
+        var id = Guid.NewGuid().ToString();
+        Console.WriteLine(obj["id"]);
+        obj["id"] = id;
+        var str = obj.ToString();
+        var str2 = JsonConvert.SerializeObject(obj);
+        Console.WriteLine("hello world");
+    }
+}
---
+++ SnakeCamelConvertion/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        string inputFilePath = @"C:\Users\jbao6\Desktop\dev\cementing\pumpschedule.json";
+        string outputFilePath = @"C:\Users\jbao6\Desktop\dev\cementing\pumpschedule_camel.json";
+        var inputcontent = File.ReadAllText(inputFilePath);
+        string snakeCaseJson = @"{
+            ""first_name"": ""Alice"",
+            ""age"": 28,
+            ""home_address"": {
+                ""street_name"": ""Main St"",
+                ""zip_code"": ""12345""
+            }
+        }";
+
+        JObject obj = JObject.Parse(inputcontent);
+        JObject camelCaseObj = ConvertKeysToCamelCase(obj);
+        var camelString = camelCaseObj.ToString();
+        // Write the converted JSON to the output file
+        Console.WriteLine(camelString);
+        // Write the converted JSON to the output file
+        File.WriteAllText(outputFilePath, camelString);
+    }
+
+    static JObject ConvertKeysToCamelCase(JObject original)
+    {
+        JObject newObj = new JObject();
+
+        foreach (var property in original.Properties())
+        {
+            string camelCaseKey = SnakeToCamel(property.Name);
+
+            if (property.Value.Type == JTokenType.Object)
+            {
+                // Recursively convert nested objects
+                newObj[camelCaseKey] = ConvertKeysToCamelCase((JObject)property.Value);
+            }
+            else if (property.Value.Type == JTokenType.Array)
+            {
+                // Handle arrays, converting objects inside the array
+                JArray array = new JArray();
+                foreach (var item in property.Value)
+                {
+                    if (item.Type == JTokenType.Object)
+                    {
+                        array.Add(ConvertKeysToCamelCase((JObject)item));
+                    }
+                    else
+                    {
+                        array.Add(item);
+                    }
+                }
+                newObj[camelCaseKey] = array;
+            }
+            else
+            {
+                newObj[camelCaseKey] = property.Value;
+            }
+        }
+
+        return newObj;
+    }
+
+    static string SnakeToCamel(string snake)
+    {
+        var parts = snake.Split('_');
+        if (parts.Length == 1)
+            return parts[0];
+
+        for (int i = 1; i < parts.Length; i++)
+        {
+            if (parts[i].Length > 0)
+                parts[i] = char.ToUpper(parts[i][0], CultureInfo.InvariantCulture) + parts[i].Substring(1);
+        }
+        return string.Join("", parts);
+    }
+}
